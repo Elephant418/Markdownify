a613f14b3cb7054920af6f32830a52945f45dffb
Peter Kruithof <pkruithof@gmail.com>
2012-08-08 14:02:02 +0200

    Initial commit

---
+++ DependencyInjection/PKMarkdownifyExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PK\MarkdownifyBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class PKMarkdownifyExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ PKMarkdownifyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PK\MarkdownifyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PKMarkdownifyBundle extends Bundle
+{
+}
