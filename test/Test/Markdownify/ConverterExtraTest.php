be71f69b86029fdd2a2ce08f0738576845489527
Thomas ZILLIOX <thomas@zilliox.me>
2013-05-11 02:47:42 +0200

    Add heading test cases

--- test/Test/Markdownify/ConverterExtraTest.php
+++ test/Test/Markdownify/ConverterExtraTest.php
@@ -8,13 +8,14 @@ use Markdownify\ConverterExtra;
 
 require_once(__DIR__ . '/../../../vendor/autoload.php');
 
-class ConverterExtraTest extends ConverterTest
+class ConverterExtraTest extends \PHPUnit_Framework_TestCase
 {
 
 
     /* ATTRIBUTES
      *************************************************************************/
     protected $converter;
+
 
 
     /* UTILS
@@ -25,10 +26,79 @@ class ConverterExtraTest extends ConverterTest
     }
 
 
-    /* TEST METHODS
+    /* HEADING TEST METHODS
      *************************************************************************/
-    public function testHeadingConversion_withAnchor()
+    public function testHeadingConversion_Level1()
     {
-        $this->assertEquals('# Heading1 {#anchor}', $this->converter->parseString('<h1 id="anchor">Heading1</h1>'));
+        echo get_class($this).PHP_EOL;
+        $this->_testHeadingConversionGeneric(1);
+    }
+
+    public function testHeadingConversion_Level1_withId()
+    {
+        $this->_testHeadingConversionGeneric(1, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level2()
+    {
+        $this->_testHeadingConversionGeneric(2);
+    }
+
+    public function testHeadingConversion_Level2_withId()
+    {
+        $this->_testHeadingConversionGeneric(2, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level3()
+    {
+        $this->_testHeadingConversionGeneric(3);
+    }
+
+    public function testHeadingConversion_Level3_withId()
+    {
+        $this->_testHeadingConversionGeneric(3, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level4()
+    {
+        $this->_testHeadingConversionGeneric(4);
+    }
+
+    public function testHeadingConversion_Level4_withId()
+    {
+        $this->_testHeadingConversionGeneric(4, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level5()
+    {
+        $this->_testHeadingConversionGeneric(5);
+    }
+
+    public function testHeadingConversion_Level5_withId()
+    {
+        $this->_testHeadingConversionGeneric(5, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level6()
+    {
+        $this->_testHeadingConversionGeneric(6);
+    }
+
+    public function testHeadingConversion_Level6_withId()
+    {
+        $this->_testHeadingConversionGeneric(6, array('id' => 'idAttribute'));
+    }
+
+    protected function _testHeadingConversionGeneric($level, $attributes=array())
+    {
+        $innerHTML = 'Heading '.$level;
+        $attributesHTML = '';
+        $md = str_pad('', $level, '#').' '.$innerHTML;
+        if (isset($attributes['id'])){
+            $md .= ' {#'.$attributes['id'].'}';
+            $attributesHTML .= ' id="'.$attributes['id'].'"';
+        }
+        $html = '<h'.$level.' '.$attributesHTML.'>'.$innerHTML.'</h'.$level.'>';
+        $this->assertEquals($md, $this->converter->parseString($html));
     }
 }
--- test/Test/Markdownify/ConverterTest.php
+++ test/Test/Markdownify/ConverterTest.php
@@ -25,10 +25,80 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
     }
 
 
-    /* TEST METHODS
+    /* HEADING TEST METHODS
      *************************************************************************/
     public function testHeadingConversion_Level1()
     {
-        $this->assertEquals('# Heading1', $this->converter->parseString('<h1>Heading1</h1>'));
+        echo get_class($this).PHP_EOL;
+        $this->_testHeadingConversionGeneric(1);
+    }
+    
+    public function testHeadingConversion_Level1_withId()
+    {
+        $this->_testHeadingConversionGeneric(1, array('id' => 'idAttribute'));
+    }
+    
+    public function testHeadingConversion_Level2()
+    {
+        $this->_testHeadingConversionGeneric(2);
+    }
+
+    public function testHeadingConversion_Level2_withId()
+    {
+        $this->_testHeadingConversionGeneric(2, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level3()
+    {
+        $this->_testHeadingConversionGeneric(3);
+    }
+
+    public function testHeadingConversion_Level3_withId()
+    {
+        $this->_testHeadingConversionGeneric(3, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level4()
+    {
+        $this->_testHeadingConversionGeneric(4);
+    }
+
+    public function testHeadingConversion_Level4_withId()
+    {
+        $this->_testHeadingConversionGeneric(4, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level5()
+    {
+        $this->_testHeadingConversionGeneric(5);
+    }
+
+    public function testHeadingConversion_Level5_withId()
+    {
+        $this->_testHeadingConversionGeneric(5, array('id' => 'idAttribute'));
+    }
+
+    public function testHeadingConversion_Level6()
+    {
+        $this->_testHeadingConversionGeneric(6);
+    }
+
+    public function testHeadingConversion_Level6_withId()
+    {
+        $this->_testHeadingConversionGeneric(6, array('id' => 'idAttribute'));
+    }
+
+    protected function _testHeadingConversionGeneric($level, $attributes=array())
+    {
+        $innerHTML = 'Heading '.$level;
+        $attributesHTML = '';
+        if (isset($attributes['id'])){
+            $attributesHTML .= ' id="'.$attributes['id'].'"';
+            $md = '<h'.$level.' '.$attributesHTML.'>'.PHP_EOL.'  '.$innerHTML.PHP_EOL.'</h'.$level.'>';
+        } else {
+            $md = str_pad('', $level, '#').' '.$innerHTML;
+        }
+        $html = '<h'.$level.' '.$attributesHTML.'>'.$innerHTML.'</h'.$level.'>';
+        $this->assertEquals($md, $this->converter->parseString($html));
     }
 }
