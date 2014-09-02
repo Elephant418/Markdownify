01c6b9cb10feb5cca212a3cdd05307524d1c20ff
Thomas ZILLIOX <thomas@zilliox.me>
2014-09-02 22:02:33 +0200

    Fix HeaderConverter text escaping

--- src/MarkdownConverter/HeaderMarkdownConverter.php
+++ src/MarkdownConverter/HeaderMarkdownConverter.php
@@ -13,8 +13,9 @@ class HeaderMarkdownConverter extends NodeConverter
      *************************************************************************/
     protected $tagList = array('h1', 'h2', 'h3', 'h4', 'h5', 'h6');
     protected $escapingRegexList = array(
-        array('@^#(#{0,5} )@U', '\#$1'),
-        array('@^===@U', '\=$1')
+        array('@^#(?#{0,5} )@', '\#$1'),
+        array('@^===@m', '\==='),
+        array('@^---@m', '\---')
     );
 
 
